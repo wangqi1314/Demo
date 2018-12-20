d440dccdfc4e97038a2c795972624096f8956626
wangqi <18700968429@163.com>
2018-12-20 14:35:13 +0800

    提交测试工程

---
+++ src/test/java/DateTypeDemo.java
@@ -0,0 +1,26 @@
+
+public class DateTypeDemo {
+	
+	
+	
+	public static void main(String[] args) {
+
+
+			int a=1;
+			int b=a++;
+			System.out.println("a="+a+",b="+b);
+			int a2=1;
+			int b2=++a2;
+			System.out.println("a2="+a2+",b2="+b2);
+			
+			int a3=1;
+			a3=a3++;
+			System.out.println("a3="+a3);
+
+	
+			int num = 10;
+			int num2=num % 2 ==0?1:2;
+			System.out.println(num2);
+	}
+
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class test {
+
+	
+	
+	public static void main(String[] args) {
+		String str = "1112223333";
+//		String regEx = "^(\\d)\\1{3}$";
+		String regEx="^.*\\d{4}.*$"; 
+		Pattern pattern = Pattern.compile(regEx);
+		Matcher matcher = pattern.matcher(str);
+		boolean rs = matcher.matches();
+		System.out.println(rs);
+		
+	}
+}
